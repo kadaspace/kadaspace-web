d939013baf2db5fc6d00825cac66e4006254b984
Martin Mwangi <mwangimartin1904@gmail.com>
2024-11-09 11:39:42 +0300

    Setup frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+
+      <h1 className="text-3xl font-bold">Kada Space</h1>
+
+    </div>
+  );
+}
---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,7 @@
+
+export const siteConfig = {
+	name: "Kadaspace",
+	description: "Kadaspace is a platform for developers to share their knowledge and grow their careers.",
+	url: "https://kadaspace.com",
+	ogImage: "https://kadaspace.com/og-image.png",
+};
